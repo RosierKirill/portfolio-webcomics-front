3f3b1fdd0855f8737028d6645ecebda31fc5ee0d
Kirill.rosier <kirill.rosier@lyceeastier.com>
2025-04-16 11:16:19 +0200

    Api resolve

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
   useEffect(() => {
     const fetchCharacters = async () => {
       try {
-        const baseURL = process.env.REACT_APP_API_URL || 'https://portfolio-webcomics-back-production.up.railway.app';
+        const API_URL = process.env.REACT_APP_API_URL || 'https://portfolio-webcomics-back-production.up.railway.app';
 
         const response = await axios.get(`${API_URL}/characters`);
         setCharacters(response.data);
